4d004a53699f3598d5a392d90c6e38c42b418182
Siva Sankar <attilisiva459@gmail.com>
2026-08-31 17:45:43 +0530

    resolve the cors issue

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.api.webhook_controller import router as webhook_router
+from app.api.payments import router as payments_router
+from app.api.audit_logs import router as audit_logs_router
+
+from app.database import create_tables
+
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(webhook_router)
+app.include_router(payments_router)
+app.include_router(audit_logs_router)
+
+create_tables()
+
+
 @app.get("/health")
 def health_check():
-    return{"status" : "Recovery agent is running"}
+    return {"status": "Recovery agent is running"}
